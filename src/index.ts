6b7dbfb18ddbbe1bc2df967b9b85b4dc1583f7a1
michael_hoffman <michael.hoffman@nielsen.com>
2024-06-10 18:03:22 +0300

    debounce

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,42 @@
-console.log('Hello World')
+// console.log('Hello World')
+
+
+// Question 3:
+// A debounced function postpones its execution by t milliseconds each time it is invoked.
+// If called again during this interval, the previous invocation is discarded.
+
+// user typ M
+// do timer that after t MS will call the function.
+// user typ I, check if previous timer didnt finish, if so reset the t MS. if not, also reset the t MS.
+
+//
+// function debounce(func, t) {
+//     // Implement your debounce function here
+//     // const check = setInterval()
+//     if (timeoutPoint) {
+//
+//     }
+//     const timeoutPoint = setTimeout(func, t)
+//     // clearInterval()
+//     // intervalPointer.
+// }
+
+const debounce = (mainFunction, delay) => {
+    // Declare a variable called 'timer' to store the timer ID
+    let timer;
+
+    // Return an anonymous function that takes in any number of arguments
+    return function (...args) {
+        // Clear the previous timer to prevent the execution of 'mainFunction'
+        clearTimeout(timer);
+
+        // Set a new timer that will execute 'mainFunction' after the specified delay
+        timer = setTimeout(() => {
+            mainFunction(...args);
+        }, delay);
+    };
+};
+
+
+debounce(console.log, 10)(3333)
+debounce(console.log, 10)(3333)
